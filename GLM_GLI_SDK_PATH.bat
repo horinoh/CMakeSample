1b6ff232f8a6deb1b137b9a059a3682799c39f79
horinoh <development@horino.sakura.ne.jp>
2016-09-03 18:26:45 +0900

    環境変数用バッチ

---
+++ GLM_GLI_SDK_PATH.bat
@@ -0,0 +1,2 @@
+@setx GLM_SDK_PATH "C:\Users\devel\Documents\GitHub\glm"
+@setx GLI_SDK_PATH "C:\Users\devel\Documents\GitHub\gli"
--- VK/GLMConfig.cmake
+++ VK/GLMConfig.cmake
@@ -1,4 +1,3 @@
-#set(GLM_INCLUDE_DIRS $ENV{GLM_SDK_PATH})
-set(GLM_INCLUDE_DIRS "C:\\Users\\devel\\Documents\\GitHub\\glm")
+set(GLM_INCLUDE_DIRS $ENV{GLM_SDK_PATH})
 
 mark_as_advanced(GLM)
