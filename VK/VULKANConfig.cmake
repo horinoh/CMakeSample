895b3c09d7a4e9c57a61a08e866d5eca1f33201b
horinoh <development@horino.sakura.ne.jp>
2016-09-12 22:21:02 +0900

    修正

--- VK/VULKANConfig.cmake
+++ VK/VULKANConfig.cmake
@@ -1,6 +1,6 @@
 set(VULKAN_INCLUDE_DIRS $ENV{VK_SDK_PATH}/Include)
 set(VULKAN_LIBRARY_DIRS $ENV{VK_SDK_PATH}/Bin)
-set(VULKAN_LIBRARIES vulkan-1)
+set(VULKAN_LIBRARIES debug vulkan-1 optimized vulkan-1)
 if(MSVC)
     set(VULKAN_DEFINITIONS -DVK_USE_PLATFORM_WIN32_KHR)
 endif()
